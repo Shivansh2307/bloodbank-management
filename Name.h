fdb07396b77c3acec705788c812350f8322bbfe0
Shivansh2307 <shivansh3.14159@gmail.com>
2022-06-10 23:44:58 +0530

    Initial Commit

---
+++ Name.h
@@ -0,0 +1,23 @@
+#ifndef NAME_H_INCLUDED
+#define NAME_H_INCLUDED
+#include<string>
+#include<iostream>
+using namespace std;
+class Name
+{
+    public:
+    string name;
+    string get_name();
+    void set_name( string);
+};
+void Name::set_name(string n)
+{
+    name=n;
+}
+string Name::get_name()
+{
+    return name;
+}
+
+#endif // NAME_H_INCLUDED
+
